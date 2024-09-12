d82f83ac7166dd84c8195241d3aa15fdcd8f7006
Ramazan <ramazan.abdullayev@bsu.edu.az>
2024-09-12 20:04:58 +0400

    87. Closest Value to Min, Max, and Mean

--- PlusPlusC.cpp
+++ PlusPlusC.cpp
@@ -6,38 +6,47 @@ using namespace std;
 
 int main() {
 
-	int size;
-	cin >> size;
-	
-	int sequence[size];
-	for(int i = 0; i < size; ++i) {
+	int length;
+	cin >> length;
+
+	int sequence[length];
+	for(int i = 0; i < length; ++i) {
 		cin >> sequence[i];
 	}
+
+	int x;
+	cin >> x;
+
+
+	int arithmeticMean = 0;
+	int max = sequence[0];
+	int min = sequence[0];
 	
-	
-	int counter = 0;
-	int maxPortion = 0;
-	
-	for(int i = 1; i < size; ++i) {				// Length of Largest Ascending Portion
-			
-		if(sequence[i - 1] < sequence[i]) {
-			counter++;
-		} else {
-			if(maxPortion < counter) {
-				maxPortion = counter;
-			}
-			counter = 0;
+	for(int i = 0; i < length; ++i) {
+		arithmeticMean += sequence[i];
+		if(max < sequence[i]) {
+			max = sequence[i];
+		} else if(min > sequence[i]) {
+			min = sequence[i];
 		}
-			
 	}
 
-	if(maxPortion < counter) {
-		maxPortion = counter;
-	}
-	
-	cout << "Length of Largest Ascending Portion: " << maxPortion << endl;
+	arithmeticMean = arithmeticMean / length;
 
-	
+	cout << max << endl;
+	cout << arithmeticMean << endl;
+	cout << min << endl;
+	cout << x << endl;
+
+	if(min <= x && x < (arithmeticMean / 2)) {			// 87. Closest Value to Min, Max, and Mean
+		cout << "Minimum" << endl;
+	} else if((arithmeticMean / 2 + arithmeticMean) < x && x <= max) {
+		cout << "Maximum" << endl;
+	} else {
+		cout << "Mean" << endl;
+	}
+
+
 	return 0;
 
 }
