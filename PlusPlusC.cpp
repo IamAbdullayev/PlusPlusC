2497244c78a9c73cb781339503f4c056a61d11ab
Ramazan <ramazan.abdullayev@bsu.edu.az>
2024-09-15 00:34:34 +0400

    Reverse Array Elements from K to L

--- PlusPlusC.cpp
+++ PlusPlusC.cpp
@@ -14,19 +14,19 @@ int main() {
 		cin >> sequence[i];
 	}
 
-	int K;
+	int K, L;
 	cin >> K;
+	cin >> L;
 
 
-	if(1 <= K && K <= length) {
+	if((1 <= K && K <= length) && (K <= L && L <= length) && (1 <= length && length <= 100)) {
 
 		for(int i = 0; i < length; ++i) {
 
-			if(K + i <= ((length + K) / 2)) {
+			if(K + i <= ((L + K) / 2)) {
 				int temp = sequence [K - 1 + i];
-				sequence[K - 1 + i] = sequence[length - 1 - i];			// Reverse Array Elements from K to N
-
-				sequence[length - 1 - i] = temp;
+				sequence[K - 1 + i] = sequence[L - 1 - i];					// Reverse Array Elements from K to L
+				sequence[L - 1 - i] = temp;
 			}
 
 			cout << sequence[i] << " ";
@@ -35,7 +35,7 @@ int main() {
 
 	} else {
 
-		cout << "1 <= K <= " << length << endl;
+		cout << "1 <= length <= 100; " << endl << "1 <= K <= L <= " << length << endl;
 
 	}
 	
