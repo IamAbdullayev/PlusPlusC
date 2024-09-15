f48c536f6ab9094dc03ed25a3d933e0978a50db5
Ramazan <ramazan.abdullayev@bsu.edu.az>
2024-09-15 12:59:55 +0400

    2# Cyclically Shift Array Elements to the Right

--- PlusPlusC.cpp
+++ PlusPlusC.cpp
@@ -9,34 +9,32 @@ int main() {
 	int length;
 	cin >> length;
 
-	int sequence[length];
-	for(int i = 0; i < length; ++i) {
-		cin >> sequence[i];
-	}
-
-
 	if(1 <= length && length <= 100) {
+		int sequence[length];
+		for(int i = 0; i < length; ++i) {
+			cin >> sequence[i];
+		}
 
-		for(int i = length - 1; i > 0; --i) {				// Cyclically Shift Array Elements to the Right
 
-			int temp = sequence[i];
+		int lastElement = sequence[length - 1];				// 2# Cyclically Shift Array Elements to the Right
+		for(int i = length - 1; i > 0; --i) {	
+
 			sequence[i] = sequence[i - 1];
-			sequence[i - 1] = temp;
 
 		}
-		
+		sequence[0] = lastElement;
+
+
+		for (int i = 0; i < length; i++) {
+			cout << sequence[i] << " ";
+		}
+		cout << endl;
+
+
 	} else {
-
 		cout << "length = " << length <<"; 1 <= length <= 100;";
-
 	}
-
-	for (int i = 0; i < length; i++) {
-		cout << sequence[i] << " ";
-	}
-	cout << endl;
 
 
 	return 0;
-
 }
