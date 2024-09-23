4f4268fe5cbb150116b38a09ff53dc837d663e7f
Ramazan <ramazan.abdullayev@bsu.edu.az>
2024-09-24 00:47:16 +0400

    2# String Copy using Pointer

--- PlusPlusC.cpp
+++ PlusPlusC.cpp
@@ -13,11 +13,14 @@ int main() {
 
 	char* ptrSource = source;
 	char* ptrDestination = destination;
+	int counter = 0;
 
-	for(size_t i = 0; *(ptrSource + i) >= 0; ++i) {
-		*(ptrDestination + i) = *(ptrSource + i);
+	for(size_t i = 0; *(ptrSource + i) != 0; ++i) {			// 2# String Copy using Pointer
+		ptrDestination[i] = ptrSource[i];
+		counter++;
 	}
-	
+	ptrDestination[counter] = 0;
+
 	cout << "Copied string: " << ptrDestination << endl;
 
 
