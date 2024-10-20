e48344e3f8837ddb0bf78d978cf9b340af750a6f
Ramazan <ramazan.abdullayev@bsu.edu.az>
2024-10-21 03:01:03 +0400

    Diagonal Sums

--- PlusPlusC.cpp
+++ PlusPlusC.cpp
@@ -8,28 +8,20 @@ int main() {
 	cin >> n;
 	
 	int matrix[n][n];
-	for (int i = 0; i < n; ++i) {				// Diagonal and Above Zeroing
+	for (int i = 0; i < n; ++i) {			// Diagonal Sums
 		for (int j = 0; j < n; ++j) {
 			cin >> matrix[i][j];
 		}
 	}
     
+	int sumMainDiagonal = 0;
+	int sumSecondaryDiagonal = 0;
 
 	for (int i = 0; i < n; ++i) {
-		for (int j = i; j < n; ++j) {
-
-			matrix[i][j] = 0;
-
-		}
+		sumMainDiagonal += matrix[i][i];
+		sumSecondaryDiagonal += matrix[i][(n - 1) - i];
 	}
 	
-	
-	for (int i = 0; i < n; ++i) {
-		for (int j = 0; j < n; ++j) {
-			cout << matrix[i][j] << " ";
-		}
-		cout << endl;
-	}
-	cout << endl;
+	cout << sumMainDiagonal << " " << sumSecondaryDiagonal << endl;
 }
 
