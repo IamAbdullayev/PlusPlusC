51fd3b65313e0072c1a484808ca99c9a402f913b
Ramazan <ramazan.abdullayev@bsu.edu.az>
2024-10-21 03:30:32 +0400

    Minimum Column Values to Array

--- PlusPlusC.cpp
+++ PlusPlusC.cpp
@@ -8,24 +8,23 @@ int main() {
 	cin >> n >> m;
 	
 	int matrix[n][m];
-	for (int i = 0; i < n; ++i) {			// Column Sum to Array
+	for (int i = 0; i < n; ++i) {			// Minimum Column Values to Array
 		for (int j = 0; j < m; ++j) {
 			cin >> matrix[i][j];
 		}
 	}
     
-	int sumColumnElementsMatrix[m];
+	int minColumnElementMatrix[m];
 
 	for (int i = 0; i < m; ++i) {
-		int sum = 0;
-		for (int j = 0; j < n; ++j) {
-			sum += matrix[j][i];
+		minColumnElementMatrix[i] = matrix[0][i];
+		for (int j = 1; j < n; ++j) {
+			if(minColumnElementMatrix[i] > matrix[j][i]) minColumnElementMatrix[i] = matrix[j][i];
 		}
-		sumColumnElementsMatrix[i] = sum;
 	}
 	
 	for (int i = 0; i < m; ++i) {
-		cout << sumColumnElementsMatrix[i] << " ";
+		cout << minColumnElementMatrix[i] << " ";
 	}
 	cout << endl;
 }
