2775c6b5e4de239a3754f9f926e6a92a235fe31c
Ramazan <ramazan.abdullayev@bsu.edu.az>
2024-10-21 22:38:47 +0400

    Monotonic Sequences in Rows

--- PlusPlusC.cpp
+++ PlusPlusC.cpp
@@ -8,24 +8,28 @@ int main() {
 	cin >> n;
 	
 	int matrix[n][n];
-	for (int i = 0; i < n; ++i) {			// Rows with Equal Elements
+	for (int i = 0; i < n; ++i) {			// Monotonic Sequences in Rows
 		for (int j = 0; j < n; ++j) {
 			cin >> matrix[i][j];
 		}
 	}
   
-	bool isAllElementEqual;
+	int countIncreasElement;
+	int countDecreasElement;
 
 	for (int i = 0; i < n; ++i) {
-		isAllElementEqual = true;
+		countIncreasElement = 0;
+		countDecreasElement = 0;
 		for (int j = 0; j < (n - 1); ++j) {
-			if(matrix[i][j] != matrix[i][j+1]) {
-				isAllElementEqual = false;
-				break;
+			if(matrix[i][j] < matrix[i][j+1]) {
+				countIncreasElement++;
+			}
+			if(matrix[i][j] > matrix[i][j+1]) {
+				countDecreasElement++;
 			}
 		}
 		
-		if (isAllElementEqual) {
+		if (countIncreasElement == (n - 1) || countDecreasElement == (n - 1)) {
 			cout << i + 1 << " ";
 		}
 	}
