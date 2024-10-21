66b0a58a6680793dc0d84c23ccb77851cfda6b58
Ramazan <ramazan.abdullayev@bsu.edu.az>
2024-10-22 00:37:16 +0400

    3# Fill 5x6 Array in a Specific Pattern

--- PlusPlusC.cpp
+++ PlusPlusC.cpp
@@ -6,13 +6,25 @@ int main() {
 
 	int n = 5;
 	int m = 6;
-	int matrix[n][m];			// Fill 5x6 Array in a Specific Pattern
+	int matrix[n][m];			// 3# Fill 5x6 Array in a Specific Pattern
 
 	int k = 1;
 
-	for (int i = 0; i < n; ++i) {
-		for (int j = 0; j < m; ++j) {
-			matrix[i][j] = k++;
+	for (int diagonals = 0; diagonals < (n + m - 1); ++diagonals) {
+		if (diagonals % 2 == 0) {
+			for (int i = 0; i <= diagonals; ++i) {
+				int j = diagonals - i;
+				if (i < n && j < m) {
+					matrix[i][j] = k++;
+				}
+			}
+		} else {
+			for (int i = 0; i <= diagonals; ++i) {
+				int j = diagonals - i;
+				if (j < n && i < m) {
+					matrix[j][i] = k++;
+				}
+			}
 		}
 	}
 
