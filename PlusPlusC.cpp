8c7d764d407296f4b469a7e49646477443833a18
Ramazan <ramazan.abdullayev@bsu.edu.az>
2024-10-21 21:14:05 +0400

    Swap Rows with Extreme Values

--- PlusPlusC.cpp
+++ PlusPlusC.cpp
@@ -7,25 +7,46 @@ int main() {
 	int n, m;
 	cin >> n >> m;
 	
-	double matrix[n][m];
-	for (int i = 0; i < n; ++i) {			// Average of Extreme Values
+	int matrix[n][m];
+	for (int i = 0; i < n; ++i) {			// Swap Rows with Extreme Values
 		for (int j = 0; j < m; ++j) {
 			cin >> matrix[i][j];
 		}
 	}
   
-	double minElementMatrix = matrix[0][0];
-	double maxElementMatrix = matrix[0][0];
+	int minElementMatrix = matrix[0][0];
+	int maxElementMatrix = matrix[0][0];
+	int maxRow(0);
+	int minRow(0);
+
 
 	for (int i = 0; i < n; ++i) {
 		for (int j = 0; j < m; ++j) {
-			if(minElementMatrix > matrix[i][j]) minElementMatrix = matrix[i][j];
-			if(maxElementMatrix < matrix[i][j]) maxElementMatrix = matrix[i][j];
+			if(minElementMatrix > matrix[i][j]) {
+				minElementMatrix = matrix[i][j];
+				minRow = i;
+			}
+
+			if(maxElementMatrix < matrix[i][j]) {
+				maxElementMatrix = matrix[i][j];
+				maxRow = i;
+			}
 		}
 	}
-	
-    cout.setf(ios::fixed);
-    cout.precision(2);
-    cout <<    ((maxElementMatrix + minElementMatrix) / (double)2) << endl;
+
+	for (int i = 0; i < m; ++i) {
+		int temp = matrix[maxRow][i];
+		matrix[maxRow][i] = matrix[minRow][i];
+		matrix[minRow][i] = temp;
+	}
+
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < m; ++j) {
+			cout <<	matrix[i][j] << " ";
+		}
+		cout << endl;
+	}
+	cout << endl;
+
 }
 
