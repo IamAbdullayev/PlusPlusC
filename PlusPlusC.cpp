74465564e386590308a4e1bcfee28e56727a61a6
Ramazan <ramazan.abdullayev@bsu.edu.az>
2024-10-22 03:03:24 +0400

    Mirror Matrix along Secondary Diagonal

--- PlusPlusC.cpp
+++ PlusPlusC.cpp
@@ -8,17 +8,17 @@ int main() {
 	cin >> n;
 
 	int matrix[n][n];
-	for (int i = 0; i < n; ++i) {			// Mirror Matrix along Main Diagonal
+	for (int i = 0; i < n; ++i) {			// Mirror Matrix along Secondary Diagonal
 		for (int j = 0; j < n; ++j) {
 			cin >> matrix[i][j];
 		}
 	}
 
 	for (int i = 0; i < n; ++i) {		
-		for (int j = i + 1; j < n; ++j) {
-			int temp = matrix[j][i];
-			matrix[j][i] = matrix[i][j];
-			matrix[i][j] = temp;
+		for (int j = 0; j < (n - 1) - i; ++j) {
+			int temp = matrix[i][j];
+			matrix[i][j] = matrix[n - j - 1][(n - i) - 1];
+			matrix[(n - j) - 1][(n - i) - 1] = temp;
 		}
 	}
 
