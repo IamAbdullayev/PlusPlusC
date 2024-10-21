b88d53917fe56953af59d4078b0412c203bc8711
Ramazan <ramazan.abdullayev@bsu.edu.az>
2024-10-21 21:21:42 +0400

    Rows with All Zeros

--- PlusPlusC.cpp
+++ PlusPlusC.cpp
@@ -4,49 +4,31 @@ int main() {
 	// system("cls");
 	// cout << boolalpha;
 
-	int n, m;
-	cin >> n >> m;
+	int n;
+	cin >> n;
 	
-	int matrix[n][m];
-	for (int i = 0; i < n; ++i) {			// Swap Rows with Extreme Values
-		for (int j = 0; j < m; ++j) {
+	int matrix[n][n];
+	for (int i = 0; i < n; ++i) {			// Rows with All Zeros
+		for (int j = 0; j < n; ++j) {
 			cin >> matrix[i][j];
 		}
 	}
   
-	int minElementMatrix = matrix[0][0];
-	int maxElementMatrix = matrix[0][0];
-	int maxRow(0);
-	int minRow(0);
-
+	bool isAllElementNull;
 
 	for (int i = 0; i < n; ++i) {
-		for (int j = 0; j < m; ++j) {
-			if(minElementMatrix > matrix[i][j]) {
-				minElementMatrix = matrix[i][j];
-				minRow = i;
-			}
-
-			if(maxElementMatrix < matrix[i][j]) {
-				maxElementMatrix = matrix[i][j];
-				maxRow = i;
+		isAllElementNull = true;
+		for (int j = 0; j < n; ++j) {
+			if(0 != matrix[i][j]) {
+				isAllElementNull = false;
+				break;
 			}
 		}
-	}
-
-	for (int i = 0; i < m; ++i) {
-		int temp = matrix[maxRow][i];
-		matrix[maxRow][i] = matrix[minRow][i];
-		matrix[minRow][i] = temp;
-	}
-
-	for (int i = 0; i < n; ++i) {
-		for (int j = 0; j < m; ++j) {
-			cout <<	matrix[i][j] << " ";
+		
+		if (isAllElementNull) {
+			cout << i + 1 << " ";
 		}
-		cout << endl;
 	}
 	cout << endl;
-
 }
 
