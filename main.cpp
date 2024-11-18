65c8da38f96180c4573daffea35928a38d3c371e
Ramazan <ramazan.abdullayev@bsu.edu.az>
2024-11-19 00:34:09 +0400

    #2 Doubly Linked List

--- main.cpp
+++ main.cpp
@@ -3,40 +3,40 @@
 
 int main()
 {
-	LinkedList list;
+	LinkedList* list = new LinkedList();
 
-	list.push_front(1.1);
-	list.push_back(2.2);
-	list.push_back(3.3);
-	list.push_front(4.4);
-	list.display();
+	list->push_front(1.1);
+	list->push_back(2.2);
+	list->push_back(3.3);
+	list->push_front(4.4);
+	list->display();
 
-	list.pop_front();
-	list.display();
+	list->pop_front();
+	list->display();
 	
-	list.pop_back();
-	list.display();
+	list->pop_back();
+	list->display();
 
-	list.insert(0, 0.0);
-	list.display();
+	list->insert(0, 0.0);
+	list->display();
 
-	list.insert(1, 0.5);
-	list.display();
+	list->insert(1, 0.5);
+	list->display();
 
-	list.insert(4, 3.3);
-	list.display();
+	list->insert(4, 3.3);
+	list->display();
 
-	list.erase(2);
-	list.display();
+	list->erase(2);
+	list->display();
 
-	list.erase(1);
-	list.display();
+	list->erase(1);
+	list->display();
 
-	list.erase(3);
-	list.display();
+	list->erase(3);
+	list->display();
 
 	cout << boolalpha;
-	cout << list.search(1.1);
+	cout << list->search(1.1);
 
 
 	return 0;
