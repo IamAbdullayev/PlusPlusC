d4c680ca3a7eb34cb6afe48d80dd22a55ea8271f
Ramazan <ramazan.abdullayev@bsu.edu.az>
2024-11-24 02:02:30 +0400

    factorial & gcd & fast_power & fibonacci

--- main.cpp
+++ main.cpp
@@ -1,27 +1,71 @@
-#include "linked_list_stack.h"
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <cstring>
+#include <math.h>
+#include <sstream>
+#include <string>
+using namespace std;
+
+int fact(int n) {
+	if (1 == n || 0 == n) return 1;
+
+	return n * fact(n - 1);
+}
+
+int greatestCommonDivisor(int a, int b) {
+	// if (0 == b || 0 == a) return a + b;
+	// if (a > b) return greatestCommonDivisor((a % b), b);
+	// return greatestCommonDivisor(a, (a % b));
+
+	if (0 == b) return a;
+	return greatestCommonDivisor(b, (a % b));
+}
+
+double fast_power(double a, int n) {
+	// if (0 == n) return 1;
+	// if ((n % 2) == 0) {
+	// 	n = n / 2;
+	// 	a *= a;
+	// }
+	// return a * fast_power(a, n - 1);
 
 
-int main()
-{
-	LinkedListStack* stack = new LinkedListStack();
+	if (0 == n) return 1;
 
-	stack->peek();
+	if (n % 2 == 0)
+		return fast_power((a * a), (n / 2));
+	else
+		return a * fast_power(a, n - 1);
+}
 
-	stack->push(1)->push(2)->push(3)->push(4)->push(5);
+int fibonacci(int n) {
+	if (n <= 1) return n;
 
-	cout << "Size: " << stack->size() << endl << endl;
+	return fibonacci(n - 1) + fibonacci(n - 2);
+}
 
-	stack->displayStack();
-	stack->pop();
-	stack->displayStack();
 
-	cout << endl;
-	stack->peek();
-	cout << endl;
 
-	stack->pop()->pop()->pop()->pop()->pop();
+int main() {
+	fact(5);
 
-	stack->displayStack();
+	int factorial;
+	factorial = fact(5);
+
+	int gcd;
+	gcd = greatestCommonDivisor(72, 36);
+
+	double exp;
+	exp = fast_power(2, 16);
+
+	int fib;
+	fib = fibonacci(10);
+
+	cout << "The factorial: " << factorial << endl;
+	cout << "The greatest common divisor: " << gcd << endl;
+	cout << "The exponentiation: " << exp << endl;
+	cout << "The fibonacci: " << fib << endl;
 
 	return 0;
 }
